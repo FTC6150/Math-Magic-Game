37a83b4d18f600e5f0e05d8ee8ccddd0eb75c145
PeterG <peterohio1@gmail.com>
2015-07-12 16:37:14 -0400

    Framework created during meeting

---
+++ app/src/main/java/mathmagic/robotrights436/com/mathmagic/DisplayableCard.java
@@ -0,0 +1,10 @@
+package mathmagic.robotrights436.com.mathmagic;
+
+/**
+ * Created by Peter on 7/12/2015.
+ */
+public class DisplayableCard {
+    private String name;
+    private String image;
+
+}
---
+++ app/src/main/java/mathmagic/robotrights436/com/mathmagic/SpellCard.java
@@ -0,0 +1,10 @@
+package mathmagic.robotrights436.com.mathmagic;
+
+/**
+ * Created by Peter on 7/12/2015.
+ */
+public class SpellCard extends DisplayableCard {
+    public SpellCard(String cardName, String imageName,) {
+
+    }
+}
